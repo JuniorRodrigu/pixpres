c9d50e12c60ca7f1c133f7aaa48f6aec422eb578
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-17 01:32:38 -0300

    teste deploy

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "770a3cb66acc4823"; }
+/******/ 		__webpack_require__.h = function() { return "de869dcfa1dba3eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ .next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "051373d11e525773"; }
+/******/ 	__webpack_require__.h = function() { return "abae0b5effa8d90c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
