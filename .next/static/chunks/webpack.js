6719c0514dc7c700569d61f53a6cd4813cca9df9
JuniorRodrigu <juniorrodrigues1717@gmail.com>
2023-06-17 02:31:14 -0300

    banco banner

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "50d9b767d2922250"; }
+/******/ 		__webpack_require__.h = function() { return "9af74e0b8a207c6d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/components/Banner/index.tsx
+++ pages/components/Banner/index.tsx
@@ -1,18 +1,46 @@
-/* eslint-disable @next/next/no-img-element */
-import Link from "next/link";
-import styles from "./styles.module.css";
+import React, { useState, useEffect } from 'react';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/storage';
+import 'firebase/compat/firestore';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
-import { Autoplay } from "swiper";
+import { Autoplay } from 'swiper';
 
+import styles from "./styles.module.css"; // Importe o arquivo CSS do Banner
 
+const firebaseConfig = {
+  apiKey: "AIzaSyDz91V8iQGtKLc8C8TzhRwGOL2soBtsMXo",
+  authDomain: "testedelyv.firebaseapp.com",
+  projectId: "testedelyv",
+  storageBucket: "testedelyv.appspot.com",
+  
+};
 
-export default function Banner()  {
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
-    return (
-        <div className={styles.container}> 
-        <Swiper
-         autoplay={{
+const db = firebase.firestore();
+
+const Banner = () => {
+  const [favoriteImages, setFavoriteImages] = useState([]);
+
+  useEffect(() => {
+    const unsubscribe = db
+      .collection('dados')
+      .where('favorito', '==', 'sim')
+      .onSnapshot((querySnapshot) => {
+        const images = querySnapshot.docs.map((doc) => doc.data().imageUrl);
+        setFavoriteImages(images);
+      });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      <Swiper
+        autoplay={{
           delay: 2500,
           disableOnInteraction: false,
         }}
@@ -20,19 +48,15 @@ export default function Banner()  {
         className={styles.swiper}
         modules={[Autoplay]}
         loop={true}
-       
       >
-        <SwiperSlide className={styles.slide} ><img src="https://firebasestorage.googleapis.com/v0/b/testedelyv.appspot.com/o/images%2Fdownload.jpg?alt=media&token=80465e59-9a1e-4dcc-9b63-52f7d96454fe" alt="" /></SwiperSlide>
-        <SwiperSlide className={styles.slide} ><img src="https://firebasestorage.googleapis.com/v0/b/testedelyv.appspot.com/o/images%2Fdownload.jpg?alt=media&token=80465e59-9a1e-4dcc-9b63-52f7d96454fe" alt="" /></SwiperSlide>
-        <SwiperSlide className={styles.slide} ><img src="https://firebasestorage.googleapis.com/v0/b/testedelyv.appspot.com/o/images%2Fdownload.jpg?alt=media&token=80465e59-9a1e-4dcc-9b63-52f7d96454fe" alt="" /></SwiperSlide>
-        
-        
-      </Swiper></div>
+        {favoriteImages.map((imageUrl) => (
+          <SwiperSlide key={imageUrl} className={styles.slide}>
+            <img src={imageUrl} alt="" className={styles.slideImage} /> {/* Adicione a classe "slideImage" aqui */}
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
+};
 
-
-    );
-
-
-
-
-}
+export default Banner;
